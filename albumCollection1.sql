95552cc9cba45258939607491b6b6f06bd8aea26
Tobias Sjöström <143131053+cybertobbe@users.noreply.github.com>
2023-11-21 20:27:32 +0100

    new file, albumCollection1
    Reworked file. adding prices to albums in INSERT INTO statement

--- albumCollection1.sql
+++ albumCollection1.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE albumCollection;
+CREATE DATABASE albumCollection1;
 
 CREATE TABLE artist(
     artistId INT NOT NULL AUTO_INCREMENT,
@@ -17,6 +17,7 @@ CREATE TABLE album(
     albumTitle VARCHAR(30),
     artistAlbumId INT NOT NULL,
     albumGenreId INT NOT NULL,
+    price INT,
     FOREIGN KEY (artistAlbumId) REFERENCES artist(artistId),
     FOREIGN KEY (albumGenreId) REFERENCES genre(genreId),
     PRIMARY KEY (albumId)
@@ -38,27 +39,23 @@ VALUES ('Classical'),
        ('Pop');
 
 
-INSERT INTO album(albumTitle, artistAlbumId, albumGenreId)
-VALUES ('Piano Concertos Nos 1-5', 1,1), -- Beethoven
-       ('Leichenfantasie', 2, 1), -- Schubert
-       ('OU812',6, 2),  -- Van Halen
-       ('Look Sharp', 4, 4),  -- Roxette
-       ('Chronic', 5, 3), -- Dr Dre
-       ('Brave new world', 3, 2); -- Iron Maiden
+INSERT INTO album(albumTitle, artistAlbumId, albumGenreId, price)
+VALUES ('Piano Concertos Nos 1-5', 1,1, 59), -- Beethoven
+       ('Leichenfantasie', 2, 1, 99), -- Schubert
+       ('OU812',6, 2, 129),  -- Van Halen
+       ('Look Sharp', 4, 4, 99),  -- Roxette
+       ('Chronic', 5, 3, 99), -- Dr Dre
+       ('Brave new world', 3, 2, 89), -- Iron Maiden
+       ('Joyride', 4,4, 89),                -- Roxette
+       ('Dreams', 6, 2, 99),                -- Van Halen
+       ('Run to the hills', 3, 2, 59),      -- Iron Maiden
+       ('Compton', 5, 3, 59),               -- Dr Dre
+       ('Piano Concerto No.4+5', 1, 1, 99), -- Beethoven
+       ('Dretox', 5, 3, 89),                 -- Dr Dre
+       ('Impromptus D899 & D935', 1, 1, 59), -- Beethoven
+       ('Piano Duets', 1, 1, 59),              -- Beethoven
+       ('Somewhere in Time', 3, 2, 129);    -- Iron Maiden
 
--- Add more albums
-INSERT INTO album(albumTitle, artistAlbumId, albumGenreId)
-VALUES ('Joyride', 4,4),                -- Roxette
-       ('Dreams', 6, 2),                -- Van Halen
-       ('Run to the hills', 3, 2),      -- Iron Maiden
-       ('Compton', 5, 3),               -- Dr Dre
-       ('Piano Concerto No.4+5', 1, 1), -- Beethoven
-       ('Dretox', 5, 3);                -- Dr Dre
-
-INSERT INTO album(albumTitle, artistAlbumId, albumGenreId)
-VALUES ('Impromptus D899 & D935', 1, 1),
-       ('Piano Duets', 1, 1),
-       ('Somewhere in Time', 3, 2);
 
 SELECT * FROM artist;
 SELECT * FROM album;
@@ -85,7 +82,7 @@ SELECT genre.genreName, album.albumTitle, artist.artistName
 FROM album
 INNER JOIN genre ON album.albumGenreId = genre.genreId
 INNER JOIN artist ON album.artistAlbumId = artist.artistId
-WHERE genre.genreName = 'Hiphop';
+WHERE genre.genreName = 'Classical';
 
 -- Räkna antalet artister
 SELECT COUNT(artist.artistName) AS numberOfArtists FROM artist;
@@ -101,11 +98,7 @@ LEFT JOIN album ON artist.artistId = album.artistAlbumId;
 SELECT * FROM ArtistsAndAlbums
          ORDER BY artistName;
 
--- Lägg till kolumn för pris i album.
-ALTER TABLE album
-ADD price INT;
--- Kolla att det kolumn för pris blivit tillagd.
-SELECT * FROM album;
+
 
 -- Transaction uppdatera album kolumn pris med priser
 
@@ -116,4 +109,4 @@ DELETE FROM genre WHERE genreID = '2';
 
 
 
-DROP DATABASE albumCollection;
+DROP DATABASE albumCollection1;
